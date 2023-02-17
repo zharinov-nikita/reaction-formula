273c8b420e8ff9da5d0b4e5d3d85c8d643d76b89
Nikita <zharinov.nicita@gmail.com>
2023-02-17 17:51:19 +0700

    release/2.0.0 validator library

--- src/types/validator.type.ts
+++ src/types/validator.type.ts
@@ -1,17 +1,2 @@
-import {
-  ValidatorIsEmail,
-  ValidatorIsEmailProps,
-  ValidatorIsEmpty,
-  ValidatorIsEmptyProps,
-  ValidatorIsMaxLength,
-  ValidatorIsMaxLengthProps,
-  ValidatorIsMinLength,
-  ValidatorIsMinLengthProps,
-} from '../validators'
-
-export interface TypeValidator {
-  isEmail: (props: ValidatorIsEmailProps) => ValidatorIsEmail
-  isEmpty: (props: ValidatorIsEmptyProps) => ValidatorIsEmpty
-  isMaxLength: (props: ValidatorIsMaxLengthProps) => ValidatorIsMaxLength
-  isMinLength: (props: ValidatorIsMinLengthProps) => ValidatorIsMinLength
-}
+import validator from 'validator'
+export type TypeValidator = typeof validator
